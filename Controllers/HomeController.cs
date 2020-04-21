fa744424867c0b91b328961d27277b3aaa98a5f4
Juan Daniel Mata Lopez <juan100mata@gmail.com>
2020-04-21 05:47:00 -0500

    Subir a gear host

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            DataTable dt;
+            dt = BaseHelper.ejecutarConsulta("SELECT * FROM VIDEO", CommandType.Text);
+            ViewData["tablaVideo"] = dt.Rows.Count;
+            return View();
+        }
+
+    }
+}
